0521c656b8912414ac6ce92bd8040edefee844db
MaryamArif1811 <maryamarif1811@gmail.com>
2026-02-08 10:44:03 +0400

    Refactor authentication logic to use anonymous user

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -1,12 +1,11 @@
-import { compare } from "bcrypt-ts";
 import NextAuth, { type DefaultSession } from "next-auth";
 import type { DefaultJWT } from "next-auth/jwt";
 import Credentials from "next-auth/providers/credentials";
-import { DUMMY_PASSWORD } from "@/lib/constants";
-import { createAnonymousUser, getUser } from "@/lib/db/queries";
+
+import { createAnonymousUser } from "@/lib/db/queries";
 import { authConfig } from "./auth.config";
 
-export type UserType = "guest" | "regular";
+export type UserType = "guest";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
@@ -18,7 +17,6 @@ declare module "next-auth" {
 
   interface User {
     id?: string;
-    email?: string | null;
     type: UserType;
   }
 }
@@ -39,36 +37,10 @@ export const {
   ...authConfig,
   providers: [
     Credentials({
-      credentials: {},
-      async authorize({ email, password }: any) {
-        const users = await getUser(email);
-
-        if (users.length === 0) {
-          await compare(password, DUMMY_PASSWORD);
-          return null;
-        }
-
-        const [user] = users;
-
-        if (!user.password) {
-          await compare(password, DUMMY_PASSWORD);
-          return null;
-        }
-
-        const passwordsMatch = await compare(password, user.password);
-
-        if (!passwordsMatch) {
-          return null;
-        }
-
-        return { ...user, type: "regular" };
-      },
-    }),
-    Credentials({
       id: "guest",
       credentials: {},
       async authorize() {
-        const [guestUser] = await createGuestUser();
+        const [guestUser] = await createAnonymousUser();
         return { ...guestUser, type: "guest" };
       },
     }),
@@ -79,7 +51,6 @@ export const {
         token.id = user.id as string;
         token.type = user.type;
       }
-
       return token;
     },
     session({ session, token }) {
@@ -87,7 +58,6 @@ export const {
         session.user.id = token.id;
         session.user.type = token.type;
       }
-
       return session;
     },
   },
